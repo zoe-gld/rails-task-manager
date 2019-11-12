3e39f8402ed099d28c3d81e4359caab9aa70e093
Zoe Goldschmidt <zoe.gld@gmail.com>
2019-11-12 15:12:02 +0000

    rails task manager

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_12_122624) do
+ActiveRecord::Schema.define(version: 2019_11_12_123336) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "details"
-    t.boolean "completed"
+    t.boolean "completed", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
